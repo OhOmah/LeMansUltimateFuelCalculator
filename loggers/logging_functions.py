5eb9f7c65a6221b2125cbc157d20f75308696f4c
Omar Zaffar <omar.zaffarr@gmail.com>
2026-04-17 13:40:08 -0700

    Started proper architecture of logging script

---
+++ loggers/logging_functions.py
@@ -0,0 +1,31 @@
+'''
+PURPOSE:
+Stores all functions in one python file to keep main script easy to read.
+'''
+import lmu_data
+
+def defineData():
+    info = lmu_data.SimInfo()
+    tele_data = info.LMUData.telemetry
+    scor_data = info.lmuData.scoring
+
+    player_index = tele_data.playerVehicleIdx
+    player_tele_data = tele_data.telemInfo[player_index]
+    player_scor_data = scor_data.vehScoringInfo[player_index]
+
+    return player_scor_data, player_tele_data, tele_data, scor_data
+
+def closeData(player_scor_data=player_scor_data, 
+            player_tele_data=player_tele_data, 
+            tele_data=tele_data, 
+            scor_data=scor_data,):
+    '''
+    PURPOSE: 
+    Takes data steams and closes 
+    '''      
+    tele_data = None
+    scor_data = None
+    player_tele_data = None
+    player_scor_data = None
+
+    return None
