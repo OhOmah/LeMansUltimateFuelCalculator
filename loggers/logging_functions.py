c1e09c77bb4e0453f0eb38cbe7c8087055274e52
Omar Zaffar <omar.zaffarr@gmail.com>
2026-04-20 08:03:13 -0700

    defined raw data functions

--- loggers/logging_functions.py
+++ loggers/logging_functions.py
@@ -30,7 +30,56 @@ def closeData(player_scor_data=player_scor_data,
 
     return None
 
-def pullPlayerName(player_scor_data = player_scor_data):
+def pullPlayerName(player_scor_data:str) -> str:
     name = player_scor_data.mDriverName
     return name
+
+def pullLapsCompleted(player_scor_data:int) -> int:
+    lapsCompleted = player_scor_data.mTotalLaps
+    return lapsCompleted
+
+def pitsIndicator(player_scor_data:bool) -> bool:
+    inPits = player_scor_data.mInPits
+    return inPits
+
+def pullSector(player_scor_data:int) -> int:
+    sector = player_scor_data.mSector
+    return sector
+
+def pullStartFinishDist(player_scor_data:float) -> float:
+    lapDist = player_sccor_data.mLapDist
+    return lapDist
+
+def pullLastLapTime(player_sccor_data:float) -> float:
+    # logs in seconds
+    lastLapTime = player_scor_data.mLastLapTime
+    return lastLapTime
+
+def pullFuelUsed(player_tele_data:float) -> float:
+    fuelUsed = player_tele_data.mFuel
+    return fuelUsed
+
+def pullFuelCapacity(player_tele_data:float) -> float:
+    fuelCapacity = player_tele_data.mFuelCapacity
+    return fuelCapacity
+
+def pullTireWear(player_tele_data:float) -> float:
+    # 0=front left, 1=front right, 2=rear left, 3=rear right
+    frontLeft = player_tele_data.mWheels[0].mWear
+    frontRight = player_tele_data.mWheels[1].mWear
+    rearLeft = player_tele_data.mWheels[2].mWear
+    rearRight = player_tele_data.mWheels[3].mWear
+    return frontLeft, frontRight, rearLeft, rearRight
+
+def pullSteeringAngle(player_tele_data:float) -> float:
+    steeringAngle = player_tele_data.mUnfilteredSteering
+    return steeringAngle
+
+def pullBrakePosition(player_tele_data:float) -> float:
+    brakePosition = player_tele_data.mUnfilteredBrake
+    return brakePosition
+
+def pullThrottlePosition(player_tele_data:float) -> float:
+    throttlePosition = player_tele_data.mUnfilteredThrottle
+    return throttlePosition
 
